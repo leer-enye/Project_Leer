214d8ba1426bf90f695f2a51e6014da5134b5a85
teenoh <ugbalaachinike@gmail.com>
2019-04-24 22:17:14 +0100

    custom page to test antd working

---
+++ pages/admin/profile.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Button } from 'antd';
+
+const Profile = () => (
+    <div>Profile Page
+    <Button>Press me</Button>
+    </div>
+);
+
+export default Profile;
