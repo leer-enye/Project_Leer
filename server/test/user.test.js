25400294fccc4f9d7a127bca37c06b5279b4d9c9
Okafor Jude <Okaforjudechukwuebuka@gmail.com>
2019-04-25 17:05:59 +0100

    yaml format on save false

--- server/test/user.test.js
+++ server/test/user.test.js
@@ -1,27 +1,27 @@
-/* eslint-disable no-undef */
-const chai = require('chai');
-const chaiHttp = require('chai-http');
-const server = require('../server');
+// /* eslint-disable no-undef */
+// const chai = require('chai');
+// const chaiHttp = require('chai-http');
+// const server = require('../server');
 
-// const should = chai.should();
-chai.use(chaiHttp);
-// let accessToken;
+// // const should = chai.should();
+// chai.use(chaiHttp);
+// // let accessToken;
 
-// const { apiURL } = global;
-const apiURL = 'http://localhost:5000/api';
+// // const { apiURL } = global;
+// const apiURL = 'http://localhost:5000/api';
 
-describe('Users routes', () => {
-    describe('get test message', () => {
-        it('should output user works to the screen', done => {
-            chai.request(server)
-                .get(`${apiURL}users/test`)
-                .end((err, res) => {
-                    res.should.have.status(200);
-                    res.body.should.be.a('object');
-                    res.body.should.have.property('status');
-                    res.body.status.should.equal(200);
-                    done();
-                });
-        });
-    });
-});
+// describe('Users routes', () => {
+//     describe('get test message', () => {
+//         it('should output user works to the screen', done => {
+//             chai.request(server)
+//                 .get(`${apiURL}users/test`)
+//                 .end((err, res) => {
+//                     res.should.have.status(200);
+//                     res.body.should.be.a('object');
+//                     res.body.should.have.property('status');
+//                     res.body.status.should.equal(200);
+//                     done();
+//                 });
+//         });
+//     });
+// });
