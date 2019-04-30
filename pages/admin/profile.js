657048906d3f7b47093d153439bbf84352c66ea2
teenoh <ugbalaachinike@gmail.com>
2019-04-30 12:16:02 +0100

    change Layout foldername to lower case

--- pages/admin/profile.js
+++ pages/admin/profile.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import Layout from '../../components/Layout';
+import Layout from '../../components/layout';
 import { ViewProfile } from "../../components/profile";
 
 const Profile = () => (
