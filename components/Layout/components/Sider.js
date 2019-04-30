e12c59aa0b82bcf916d6b0c7688eb9667d4bc04f
teenoh <ugbalaachinike@gmail.com>
2019-04-30 12:16:55 +0100

    custom Sider component with reusable menu items

---
+++ components/Layout/components/Sider.js
@@ -0,0 +1,60 @@
+import React from 'react';
+import Link from 'next/link';
+import { Menu, Layout, Typography, Avatar, Icon } from 'antd';
+
+import { SIDER_LINKS } from '../constants';
+
+const { Sider } = Layout;
+const { Title, Text } = Typography;
+
+const CustomSider = ({ selectedMenuItem }) => {
+	return (
+		<Sider
+			className="sider"
+			breakpoint="md"
+			collapsedWidth={0}
+			onBreakpoint={broken => {
+				console.log(broken);
+			}}
+			onCollapse={(collapsed, type) => {
+				console.log(collapsed, type);
+			}}
+		>
+			<Title level={2} className="sider__logo">
+				{' '}
+				Leer{' '}
+			</Title>
+			<div className="sider__profile">
+				<Avatar size={80} icon="user" />
+				<Text className="sider__profile-text text-white">
+					Hello Jude!
+				</Text>
+			</div>
+
+			<nav className="sider__menu">
+				<Menu
+					theme="dark"
+					mode="inline"
+					defaultSelectedKeys={[selectedMenuItem]}
+				>
+					{SIDER_LINKS.map(item => {
+						return (
+							<Menu.Item key={item.key}>
+								<Link href={item.path}>
+									<span>
+										<Icon type={item.icon} />
+										<span className="nav-text">
+											{item.name}
+										</span>
+									</span>
+								</Link>
+							</Menu.Item>
+						);
+					})}
+				</Menu>
+			</nav>
+		</Sider>
+	);
+};
+
+export default CustomSider;
--- components/Layout/index.js
+++ components/Layout/index.js
@@ -1,94 +1,16 @@
 /* eslint-disable react/destructuring-assignment */
 import React from 'react';
-import Link from 'next/link';
-import { Layout, Menu, Icon, Typography, Avatar, Breadcrumb } from 'antd';
+import { Layout, Typography, Breadcrumb } from 'antd';
+import { CustomSider } from "./components";
 import './index.scss';
 
-const { Header, Sider, Content, Footer } = Layout;
-const { Title, Text } = Typography;
+const { Header, Content, Footer } = Layout;
+const { Title } = Typography;
 
 const CustomLayout = props => (
 	<React.Fragment>
 		<Layout className="layout" hasSider>
-			<Sider
-				className="sider"
-				breakpoint="md"
-				collapsedWidth={0}
-				onBreakpoint={broken => {
-					console.log(broken);
-				}}
-				onCollapse={(collapsed, type) => {
-					console.log(collapsed, type);
-				}}
-			>
-				<Title level={2} className="sider__logo">
-					{' '}
-					Leer{' '}
-				</Title>
-				<div className="sider__profile">
-					<Avatar size={80} icon="user" />
-					<Text className="sider__profile-text text-white">
-						Hello Jude!
-					</Text>
-				</div>
-
-				<nav className="sider__menu">
-					<Menu
-						theme="dark"
-						mode="inline"
-						defaultSelectedKeys={[props.selectedMenuItem]}
-					>
-						<Menu.Item key="home">
-							<Link href="/admin">
-								<span>
-									<Icon type="dashboard" />
-									<span className="nav-text">Home</span>
-								</span>
-							</Link>
-						</Menu.Item>
-
-						<Menu.Item key="resources">
-							<Link href="/admin/resources">
-								<span>
-									<Icon type="appstore" />
-									<span className="nav-text">
-										Learning Resources
-									</span>
-								</span>
-							</Link>
-						</Menu.Item>
-
-						<Menu.Item key="practice">
-							<Link href="/admin/practice">
-								<span>
-									<Icon type="edit" />
-									<span className="nav-text">Practice</span>
-								</span>
-							</Link>
-						</Menu.Item>
-
-						<Menu.Item key="challenge">
-							<Link href="/admin/challenge">
-								<span>
-									<Icon type="thunderbolt" />
-									<span className="nav-text">
-										Challenge Friends
-									</span>
-								</span>
-							</Link>
-						</Menu.Item>
-
-						<Menu.Item key="profile">
-							<Link href="/admin/profile">
-								<span>
-									<Icon type="user" />
-									<span className="nav-text">Profile</span>
-								</span>
-							</Link>
-						</Menu.Item>
-					</Menu>
-				</nav>
-			</Sider>
+			<CustomSider selectedMenuItem={props.selectedMenuItem} />
 			<Layout>
 				<Header className="header">
 					<Title className="header__logo" level={2}>
