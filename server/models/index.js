1e4c91465c1c1281e43e31f76981da2a796ce05d
Tosin Amuda <tosinamuda@gmail.com>
2019-05-02 17:43:45 +0100

    Fix Refactored Model #9

---
+++ server/models/index.js
@@ -0,0 +1,5 @@
+const { Content } = require('../models/Content');
+const { User } = require('../models/User');
+const { Subject } = require('../models/Subject');
+
+module.exports = { Content, Subject, User };
