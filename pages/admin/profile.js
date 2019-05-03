4946076d136aa8e760c3076a9c42a60c4a54620d
teenoh <ugbalaachinike@gmail.com>
2019-05-03 11:28:06 +0100

    render viewProfile and  editProfile conditionally

--- pages/admin/profile.js
+++ pages/admin/profile.js
@@ -1,13 +1,39 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { Button, Row, Col } from 'antd';
 import Layout from '../../components/layout';
 import { components } from '../../components/profile';
 
-const { ViewProfile } = components;
+const { ViewProfile, EditProfile } = components;
 
-const Profile = () => (
-    <Layout selectedMenuItem="profile">
-        <ViewProfile />
-    </Layout>
-);
+class Profile extends Component {
+    state = { action: 'view' }
+    
+    changeAction = () => {
+        const { action } = this.state;
+        
+        if (action === 'view'){
+            return this.setState({ action: 'edit' });
+        }
+
+        return this.setState({ action: 'view' });
+    }
+
+    render() {
+        const { action } = this.state;
+        return (
+            <Layout selectedMenuItem="profile">
+                <Row>
+                    <Col span={24} style={{ marginBottom: '2rem' }}>
+                        <Button type="primary" onClick={this.changeAction}>{`${action === 'view' ? 'Edit' : 'View'} Profile`}</Button>
+                    </Col>
+                    <Col span={24}>
+                        { action === 'view' ?  <ViewProfile /> : <EditProfile /> }
+                    </Col>
+                </Row>
+            </Layout>
+        );
+    }
+
+}
 
 export default Profile;
