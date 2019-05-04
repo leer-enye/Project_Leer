eb4bcdd4be1792ba3ab20be162a0ca6091c1b4c9
Tosin Amuda <tosinamuda@gmail.com>
2019-05-04 17:17:18 +0100

    Fix Unused Lines in #9

--- server/routes/api/content.js
+++ server/routes/api/content.js
@@ -1,25 +1,13 @@
 const express = require('express');
+const contents = require('../../controllers/content');
 
 const router = express.Router();
 
-const contents = require('../../controllers/content');
-
-// eslint-disable-next-line func-names
-module.exports = function(app) {
+module.exports = app => {
     app.use('/api/contents', router);
-
-    // Create a new Note
     router.post('/', contents.create);
-
-    // Retrieve all Notes
     router.get('/', contents.findAll);
-
-    // Retrieve a single Note with contentId
     router.get('/:contentId', contents.findOne);
-
-    // Update a Note with contentId
     router.put('/:contentId', contents.update);
-
-    // Delete a Note with contentId
     router.delete('/:contentId', contents.delete);
 };
--- server/routes/api/subject.js
+++ server/routes/api/subject.js
@@ -1,24 +1,14 @@
 const express = require('express');
-
-const router = express.Router();
 const subjects = require('../../controllers/subject');
 
-// eslint-disable-next-line func-names
-module.exports = function(app) {
+const router = express.Router();
+
+module.exports = app => {
     app.use('/api/subjects', router);
-    // Create a new Note
     router.post('/', subjects.create);
-
-    // Retrieve all Notes
     router.get('/', subjects.findAll);
-
-    // Retrieve a single Note with subjectId
     router.get('/:subjectId', subjects.findOne);
-
-    // Update a Note with subjectId
     router.put('/:subjectId', subjects.update);
-
-    // Delete a Note with subjectId
     router.delete('/:subjectId', subjects.delete);
 
 };
