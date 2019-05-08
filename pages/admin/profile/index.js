1b0c5d5912fb3445fe50ab735f1ad7f4326d9949
teenoh <ugbalaachinike@gmail.com>
2019-05-08 04:55:50 +0100

    modify profile page structure to easily add styles

--- pages/admin/profile/index.js
+++ pages/admin/profile/index.js
@@ -1,17 +1,18 @@
 import React, { Component } from 'react';
 import { Button, Row, Col } from 'antd';
-import Layout from '../../components/layout';
-import { components } from '../../components/profile';
+import Layout from '../../../components/layout';
+import { components } from '../../../components/profile';
+import './index.scss';
 
 const { ViewProfile, EditProfile } = components;
 
 class Profile extends Component {
     state = { action: 'view' }
-    
+
     changeAction = () => {
         const { action } = this.state;
-        
-        if (action === 'view'){
+
+        if (action === 'view') {
             return this.setState({ action: 'edit' });
         }
 
@@ -23,16 +24,16 @@ class Profile extends Component {
         return (
             <Layout selectedMenuItem="profile">
                 <Row>
-                    <Col span={24} style={{ marginBottom: 16 }}>
-                        
-                        <Button 
-                            type="primary" 
+                    <Col span={24} className='mb-1'>
+
+                        <Button
+                            type="primary"
                             onClick={this.changeAction}>
                             {`${action === 'view' ? 'Edit' : 'View'} Profile`}
                         </Button>
                     </Col>
                     <Col span={24}>
-                        { action === 'view' ?  <ViewProfile /> : <EditProfile /> }
+                        {action === 'view' ? <ViewProfile /> : <EditProfile />}
                     </Col>
                 </Row>
             </Layout>
