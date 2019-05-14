e3908bd68073cd14eec4812ebdf39144e37cc93c
teenoh <ugbalaachinike@gmail.com>
2019-05-14 13:07:46 +0100

    add component to display challenge info

---
+++ components/challenge/components/index.js
@@ -0,0 +1,11 @@
+import CourseSelect from './CourseSelect';
+import ModeSelect from './ModeSelect';
+import OpponentSelect from './OpponentSelect';
+import ChallengeInfo from './ChallengeInfo'; 
+
+export {
+    CourseSelect,
+    ModeSelect,
+    OpponentSelect,
+    ChallengeInfo
+};
