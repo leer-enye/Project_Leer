8b27888e4a45ca7e70cecc01c8a1f38a27bdb010
teenoh <ugbalaachinike@gmail.com>
2019-05-14 13:06:14 +0100

    add component to select courses

---
+++ components/challenge/components/CourseSelect/index.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import Link from 'next/link';
+import { Typography, Row, Col, Card } from 'antd';
+import  { CHALLENGE_PAGES_HEADERS , CLASSNAMES, DEFAULT_PROPS } from '../../constants';
+import './index.scss';
+
+const { Title } = Typography;
+const { courseCard, mt4 } = CLASSNAMES;
+const { courseSelectLabel } = CHALLENGE_PAGES_HEADERS;
+
+const CourseSelect = ({ subjects, next }) =>  (
+    <section>
+        <Title level={3}>{ courseSelectLabel }</Title>
+        <Row gutter={16} className={mt4}>
+            {subjects.map(({ name, id }) => (
+                <Col key={id} span={6} md={6} xs={12}>
+                    <Link href={next}>
+                        <Card className={courseCard} hoverable>
+                            {name}
+                        </Card>
+                    </Link>
+                </Col>
+            ))}
+            
+        </Row>
+    </section>
+);
+
+CourseSelect.defaultProps = {
+    subjects: DEFAULT_PROPS.courseSelect.subjects,
+};
+
+export default CourseSelect;
