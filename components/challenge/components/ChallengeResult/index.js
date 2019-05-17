3111049800c8c34b04e5aa249a66f8e492fa0c8e
teenoh <ugbalaachinike@gmail.com>
2019-05-17 21:18:35 +0100

    refactor code in ChallengeResult

--- components/challenge/components/ChallengeResult/index.js
+++ components/challenge/components/ChallengeResult/index.js
@@ -1,35 +1,69 @@
 import React from 'react';
 import { Typography, Button } from 'antd';
+import * as constants from '../../constants';
 import './index.scss';
 
 const { Title } = Typography;
+const { 
+    BUTTON_TYPE_PRIMARY,
+    DEFAULT_PROPS,
+    CLASS_NAMES,
+    EXTRA_TEXTS,
+} = constants;
 
-const ChallengeResult = () => (
-    <div className='result-card'>
-        <Title className='text-center' level={2}> YOU WIN</Title>
-        <div className='challengers-box'>
-            <div className='challenger-info win'>
-                <img src='/static/images/uche.jpg' alt='challenger' />
-                <div className=''>
-                    <h3 className='challenger-info__name'>Uche</h3>
-                    <p className='challenger-info__score'>200</p>
-                </div>
-            </div>
-            <Title level={3} className='vs-divider'> Vs</Title>
-            <div className='challenger-info lose'>
-                <img src='/static/images/uche.jpg' alt='challenger' />
-                <div>
-                    <h3 className='challenger-info__name'>Uche</h3>
-                    <p className='challenger-info__score'>160</p>
-                </div>
-            </div>
+const {
+    challengerInfo,
+    challengerInfoName,
+    challengerInfoScore,
+    challengersBox,
+    loser,
+    mb1,
+    mr1,
+    resultCard,
+    textCenter,
+    vsDivider,
+    winner,
+} = CLASS_NAMES;
+const { greatGame, viewResults, backToHome, vs } = EXTRA_TEXTS;
+const { challengeResult } = DEFAULT_PROPS;
+const { defaultChallengers, defaultResultInfo } = challengeResult;
+
+const ChallengeResult = ({ challengers, resultInfo }) => (
+    <div className={resultCard}>
+        <Title className={textCenter} level={2}> {resultInfo} </Title>
+        <div className={challengersBox}>
+            {
+                challengers.map(({ id, image, score, status, username }, index) => (
+                    <React.Fragment key={id}>
+                        <div className={`${challengerInfo} ${(status === 'win')?winner: loser}`}>
+                            <img src={image} alt={username} />
+                            <div className=''>
+                                <h3 className={challengerInfoName}>{username}</h3>
+                                <p className={challengerInfoScore}>{score}</p>
+                            </div>
+                        </div>
+                        {
+                            (index === 0) ? 
+                                <Title className={vsDivider} level={1}> 
+                                    {vs}
+                                </Title> 
+                                : null
+                        }
+                    </React.Fragment>
+                ))
+            }
         </div>
-        <Title level={4} className='vs-divider'> Great Game! </Title>
+        <Title level={4}> {greatGame} </Title>
         <div>
-            <Button type='primary' className='mr-1 mb-1'>View Results</Button>
-            <Button >Back to Home</Button>
+            <Button type={BUTTON_TYPE_PRIMARY} className={`${mr1} ${mb1}`}>{viewResults}</Button>
+            <Button >{backToHome}</Button>
         </div>
     </div>
 );
+
+ChallengeResult.defaultProps = {
+    challengers: defaultChallengers,
+    resultInfo: defaultResultInfo,
+};
 
 export default ChallengeResult;
