89cb081f274e4903fc6e71a2567b81ca78704538
Tosin Amuda <tosinamuda@gmail.com>
2019-05-24 09:38:08 +0100

    Refactor (User) : Remove Uneccesary Codes [Fixes 14]

--- pages/user.js
+++ pages/user.js
@@ -8,39 +8,27 @@ class User extends Component {
     static async getInitialProps({ req }) {
         let isLoggedIn = false;
         let userData = {};
+
         try {
-            const headers = req
-                ? {
-                    cookie: req.headers.cookie,
-				  }
-                : undefined;
-            const res = await fetch('http://localhost:5000/api/users/users', {
+            const { cookie } = req.headers;
+            const headers = req ? { cookie } : undefined;
+            const res = await fetch('/api/users/users', {
                 headers,
             });
 
             const userRes = await res.json();
-            console.log(userRes);
             const { data } = userRes;
             const { user } = data;
             isLoggedIn = true;
             userData = user;
-            console.log(`Show user fetched. : ${userData}`);
         } catch (err) {
             console.log(err);
         }
         return { isLoggedIn, userData };
     }
 
-    constructor(props) {
-        super(props);
-        this.state = {
-            isLoggedIn: props.isLoggedIn,
-            user: props.userData,
-        };
-    }
-
     render() {
-        const { user, isLoggedIn } = this.state;
+        const { userData: user, isLoggedIn } = this.props;
         const { name, email, picture } = user;
         return (
             <Layout selectedMenuItem="profile">
