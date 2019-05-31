2a18be6350953a0bce05dd07a56e3eae9de8a8a8
Tosin Amuda <tosinamuda@gmail.com>
2019-05-31 12:21:59 +0100

    chore(socketio): pair user logic

--- server/server.js
+++ server/server.js
@@ -92,9 +92,66 @@ app.prepare()
 
         server.get('*', (req, res) => handle(req, res));
 
-        io.on('connect', socket => {
+        const queue = []; // list of sockets waiting for peers
+        const rooms = {}; // map socket.id => room
+        const names = {}; // map socket.id => name
+        const allUsers = {}; // map socket.id => socket
+
+        const findPeerForLoneSocket = socket => {
+            // this is place for possibly some extensive logic
+            // which can involve preventing two people pairing multiple times
+            if (queue) {
+                // somebody is in queue, pair them!
+                const peer = queue.pop();
+                const room = `${socket.id}#${peer.id}`;
+                // join them both
+                peer.join(room);
+                socket.join(room);
+                // register rooms to their names
+                rooms[peer.id] = room;
+                rooms[socket.id] = room;
+                // exchange names between the two of them and start the chat
+                peer.emit('chat start', { name: names[socket.id], room });
+                socket.emit('chat start', { name: names[peer.id], room });
+            } else {
+                // queue is empty, add our lone socket
+                queue.push(socket);
+            }
+        };
+        /*         io.on('connect', socket => {
             socket.emit('now', {
                 message: 'Zeit',
+            });
+        }); */
+
+        io.on('connection', socket => {
+            console.log(`User ${socket.id} connected`);
+            socket.on('login', data => {
+                names[socket.id] = data.username;
+                allUsers[socket.id] = socket;
+                // now check if sb is in queue
+                findPeerForLoneSocket(socket);
+            });
+            socket.on('message', data => {
+                const room = rooms[socket.id];
+                socket.broadcast.to(room).emit('message', data);
+            });
+            socket.on('leave room', () => {
+                const room = rooms[socket.id];
+                socket.broadcast.to(room).emit('chat end');
+                let peerID = room.split('#');
+                peerID = peerID[0] === socket.id ? peerID[1] : peerID[0];
+                // add both current and peer to the queue
+                findPeerForLoneSocket(allUsers[peerID]);
+                findPeerForLoneSocket(socket);
+            });
+            socket.on('disconnect', () => {
+                const room = rooms[socket.id];
+                socket.broadcast.to(room).emit('chat end');
+                let peerID = room.split('#');
+                peerID = peerID[0] === socket.id ? peerID[1] : peerID[0];
+                // current socket left, add the other one to the queue
+                findPeerForLoneSocket(allUsers[peerID]);
             });
         });
 
