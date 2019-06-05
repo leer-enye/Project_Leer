e159b2d7642a5bd4ee813f6b9107c2f94624dd0f
teenoh <ugbalaachinike@gmail.com>
2019-06-05 23:38:21 +0100

    refactor code

--- store/index.js
+++ store/index.js
@@ -1,19 +1,18 @@
 import { applyMiddleware, createStore  } from 'redux';
 import createSagaMiddleware from 'redux-saga';
-
+import { composeWithDevTools } from 'redux-devtools-extension';
 import rootReducer from '../components/rootReducer';
 import rootSaga from "../components/rootSaga";
 
 const bindMiddleware = middleware => {
     if (process.env.NODE_ENV !== 'production'){
-        const { composeWithDevTools } = require('redux-devtools-extension');
         return composeWithDevTools(applyMiddleware(...middleware));
     }
     return applyMiddleware(...middleware);
-}
+};
 
 function configureStore(initialState={}) {
-    const sagaMiddleware = createSagaMiddleware()
+    const sagaMiddleware = createSagaMiddleware();
     const store = createStore(
         rootReducer,
         initialState,
