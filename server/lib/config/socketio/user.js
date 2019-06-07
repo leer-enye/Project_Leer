6043ac92b2d40f47431b8e36a4231f089f378a54
Tosin Amuda <tosinamuda@gmail.com>
2019-06-07 07:28:42 +0100

    Refactor(User): Make User Object Consistent

---
+++ server/lib/config/socketio/user.js
@@ -0,0 +1,10 @@
+class User {
+    constructor(id, name, picture, socketId) {
+        this.id = id;
+        this.name = name;
+        this.picture = picture;
+        this.socketId = socketId;
+    }
+}
+
+export default User;
