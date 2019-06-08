7a04b4dc0109006aa6c4663e48331b0fe3366e06
Tosin Amuda <tosinamuda@gmail.com>
2019-06-08 14:50:17 +0100

    Refactor(Arrow Func) : Use arrow functions

--- server/lib/config/socketio/index.js
+++ server/lib/config/socketio/index.js
@@ -20,79 +20,89 @@ const { connection, message, disconnet } = SERVER_SYSTEM_EVENTS;
 
 module.exports = io => {
     const rooms = new Map(); // map user.id => roomId
-    const allUsers = new Map(); // map socket.id => socket
-    const userList = new Map(); // map user.id => user(id, name, picture, socketId)
+    const allSockets = new Map(); // map socket.id => socket
+    const allUsers = new Map(); // map user.id => user(id, name, picture, socketId)
     const availableUsers = new Map();
-    const socketUserList = new Map(); // map socket.id => user
+    const socketUsers = new Map(); // map socket.id => user
+
+    const sendAvailableUserList = socket => {
+        const userMapClone = new Map(availableUsers);
+        const { id: socketId } = socket;
+        const { id: userId } = socketUsers.get(socketId);
+        userMapClone.delete(userId);
+        const otherUsers = [...userMapClone.values()];
+        socket.emit(users, { users: otherUsers });
+    };
 
     io.on(connection, socket => {
         socket.on(login, data => {
             const { _id: userId, name, picture } = data;
             const { id: socketId } = socket;
             const user = new User(userId, name, picture, socketId);
-            userList.set(userId, user);
+            allUsers.set(userId, user);
+            socketUsers.set(socketId, user);
+            allSockets.set(socketId, socket);
             availableUsers.set(userId, user);
-            socketUserList.set(socketId, user);
-            allUsers.set(socketId, socket);
+            sendAvailableUserList(socket);
         });
         socket.on(getUser, () => {
-            const userMapClone = new Map(availableUsers);
-            const { id: socketId } = socket;
-            const { id: userId } = socketUserList.get(socketId);
-            userMapClone.delete(userId);
-            const otherUsers = [...userMapClone.values()];
-            socket.emit(users, { users: otherUsers });
+            sendAvailableUserList(socket);
         });
         socket.on(message, data => {
             const { id: socketId } = socket;
-            const { id: userId } = socketUserList.get(socketId);
+            const { id: userId } = socketUsers.get(socketId);
             const room = rooms[userId];
             socket.broadcast.to(room).emit(message, data);
         });
         socket.on(leaveRoom, () => {
             const { id: socketId } = socket;
-            const { id: userId, name, picture } = socketUserList.get(socketId);
+            const { id: userId, name, picture } = socketUsers.get(socketId);
             rooms.delete(userId);
             const user = new User(userId, name, picture, socketId);
             availableUsers.set(userId, user);
+            sendAvailableUserList(socket);
             socket.emit(challengeEnd);
         });
         socket.on(selectUser, data => {
             const { id: socketId } = socket;
-            const originatingUser = socketUserList.get(socketId);
+            const originatingUser = socketUsers.get(socketId);
             const { id: selectedUserId } = data;
-            const selectedUser = userList.get(selectedUserId);
+            const selectedUser = allUsers.get(selectedUserId);
             const { socketId: selectedUserSocketId } = selectedUser;
-            const peer = allUsers.get(selectedUserSocketId);
+            const peer = allSockets.get(selectedUserSocketId);
             const roomId = `${originatingUser.id}#${selectedUser.id}`;
 
-            // join them both
-            peer.join(roomId);
-            socket.join(roomId);
+            // temporarily join both to rooms
+            [peer, socket].forEach(item => item.join(roomId));
 
-            availableUsers.delete(originatingUser.id);
-            availableUsers.delete(selectedUser.id);
+            // Remove from avaialbe queue
+            [originatingUser, selectedUser].forEach(item =>
+                availableUsers.delete(item.id)
+            );
 
             // register user temporarirly to rooms
-            rooms.set(selectedUser.id, roomId);
-            rooms.set(originatingUser.id, roomId);
+            [originatingUser, selectedUser].forEach(item =>
+                rooms.set(item.id, roomId)
+            );
 
             peer.emit(challengeRequest, {
                 room: roomId,
                 user: originatingUser,
             });
             socket.emit(challengeRequest, { room: roomId, user: selectedUser });
+
+            [socket, peer].forEach(item => sendAvailableUserList(item));
         });
         socket.on(acceptChallenge, () => {
             // emit challengeStart
             const { id: socketId } = socket;
-            const { id: userId } = socketUserList.get(socketId);
+            const { id: userId } = socketUsers.get(socketId);
             const roomId = rooms.get(userId);
             socket.broadcast.to(roomId).emit(challengeStart);
         });
         socket.on(rejectChallenge, () => {
             const { id: socketId } = socket;
-            const { id: userId, name, picture } = socketUserList.get(socketId);
+            const { id: userId, name, picture } = socketUsers.get(socketId);
             const user = new User(userId, name, picture, socketId);
             const roomId = rooms.get(userId);
 
@@ -100,25 +110,23 @@ module.exports = io => {
                 socket.broadcast.to(roomId).emit(challengeEnd);
                 const [partyA, partyB] = roomId.split('#');
                 const peerId = partyA === userId ? partyB : partyA;
-                rooms.delete(userId);
-                rooms.delete(peerId);
-                availableUsers.set(peerId, user);
+                const peer = allUsers.get(peerId);
+                [userId, peerId].forEach(item => rooms.delete(item));
+                availableUsers.set(peerId, peer);
                 availableUsers.set(userId, user);
             }
         });
         socket.on(disconnet, () => {
             const { id: socketId } = socket;
-            const { id: userId } = socketUserList.get(socketId);
+            const { id: userId } = socketUsers.get(socketId);
             const roomId = rooms.get(userId);
 
             if (roomId) {
                 rooms.delete(userId);
                 socket.emit(challengeEnd);
             }
-            userList.delete(userId);
-            availableUsers.delete(userId);
-            socketUserList.delete(socketId);
-            allUsers.delete(socketId);
+            [allUsers, availableUsers].forEach(item => item.delete(userId));
+            [socketUsers, allSockets].forEach(item => item.delete(socketId));
         });
     });
 };
