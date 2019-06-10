cbfa867b08c8af9b19c4a45c3ccc678185fff13f
teenoh <ugbalaachinike@gmail.com>
2019-06-10 15:00:59 +0100

    remove unused code

--- components/profile/sagas.js
+++ components/profile/sagas.js
@@ -1,12 +1,7 @@
 import { all } from 'redux-saga/effects';
 
-export function* helloSaga(){
-    console.log(' Hello Sagas! ');
-    yield ;
-};
-
 export default function* (){
     yield all([
-        helloSaga(),
+        
     ]);
 };
