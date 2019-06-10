b45034f3e945c855bee3e7292345a3ce023dd2d5
teenoh <ugbalaachinike@gmail.com>
2019-06-10 14:38:28 +0100

    refactor action types and add necessary actions

--- components/auth/actions.js
+++ components/auth/actions.js
@@ -1,8 +1,25 @@
 import { 
-    UPDATE_USER_REQUEST,
-    UPDATE_USER_FULFILLED,
-    UPDATE_USER_REJECTED
+    FETCH_USER_ACTION_TYPES,
+    UPDATE_USER_ACTION_TYPES,
+    SAVE_SESSION_ACTION_TYPES,
+    REMOVE_SESSION
 } from './actionTypes';
+
+const {
+    FETCH_USER_REQUEST,
+    FETCH_USER_REJECTED,
+} = FETCH_USER_ACTION_TYPES;
+
+const {
+    UPDATE_USER_FULFILLED,
+    UPDATE_USER_REJECTED,
+    UPDATE_USER_REQUEST,
+} = UPDATE_USER_ACTION_TYPES;
+
+const {
+    SAVE_SESSION,
+    SAVE_SESSION_REQUEST,
+} = SAVE_SESSION_ACTION_TYPES;
 
 export const updateUserRequest = data => ({
     payload: data,
@@ -17,4 +34,27 @@ export const updateUserFulfilled = user => ({
 export const updateUserRejected = err => ({
     payload: err,
     type: UPDATE_USER_REJECTED,
+});
+
+export const fetchUserRequest = () => ({
+    type: FETCH_USER_REQUEST,
+});
+
+export const fetchUserRejected = () => ({
+    payload: null,
+    type: FETCH_USER_REJECTED,
+});
+
+export const saveSessionRequest = session => ({
+    payload: session,
+    type: SAVE_SESSION_REQUEST,
+});
+
+export const saveSessionAction = session => ({
+    payload: session,
+    type: SAVE_SESSION,
+});
+
+export const removeSession = () => ({
+    type: REMOVE_SESSION,
 });
