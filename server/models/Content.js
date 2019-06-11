ea7ceb9090f421cbb7e5051800e113e731e3987c
Tosin Amuda <tosinamuda@gmail.com>
2019-06-11 06:14:24 +0100

    Refactor(Prettier)

--- server/models/Content.js
+++ server/models/Content.js
@@ -3,13 +3,16 @@ const mongoose = require('mongoose');
 
 const { Schema } = mongoose;
 
-const ContentSchema = new Schema({
-    title: String,
-    resourceType: String,
-    tags: [String],
-    url: String,
-    subjectId: mongoose.Schema.Types.ObjectId,
-}, { timestamps: true });
+const ContentSchema = new Schema(
+    {
+        title: String,
+        resourceType: String,
+        tags: [String],
+        url: String,
+        subjectId: mongoose.Schema.Types.ObjectId,
+    },
+    { timestamps: true }
+);
 const Content = mongoose.model('Content', ContentSchema);
 
 module.exports = Content;
