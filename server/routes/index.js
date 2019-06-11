e2bf5a08789a90134a01311b59ce2d7eb00f0cbf
Tosin Amuda <tosinamuda@gmail.com>
2019-06-11 06:28:11 +0100

    Chore(Question) : Added CRUD API

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
-const subjects = require('../routes/api/subject');
 const contents = require('../routes/api/content');
+const questions = require('../routes/api/question');
+const subjects = require('../routes/api/subject');
 const users = require('../routes/api/user');
 
 const router = express.Router();
@@ -8,5 +9,6 @@ const router = express.Router();
 users(router);
 subjects(router);
 contents(router);
+questions(router);
 
 module.exports = router;
