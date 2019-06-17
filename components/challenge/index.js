a8fa0b283545b4a35c37e4571426d9b2923998ba
teenoh <ugbalaachinike@gmail.com>
2019-06-17 18:13:39 +0100

    code refactor

--- components/challenge/index.js
+++ components/challenge/index.js
@@ -1,5 +1,8 @@
 /* eslint-disable import/prefer-default-export */
 import * as components from './components';
 import * as constants from './constants';
+import * as actions from './actions';
+import reducers from './reducers';
+import sagas from './sagas';
 
-export { components, constants  };
+export { actions, components, constants, reducers, sagas };
