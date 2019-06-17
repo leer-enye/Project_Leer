42ef1a9b13ab1d616ef9bcc36bd8d6e1f90b7614
teenoh <ugbalaachinike@gmail.com>
2019-06-17 18:12:20 +0100

    save selected course to store and redirect to next page

--- components/challenge/components/CourseSelect/index.js
+++ components/challenge/components/CourseSelect/index.js
@@ -1,4 +1,7 @@
+/* eslint-disable jsx-a11y/click-events-have-key-events */
+/* eslint-disable jsx-a11y/no-static-element-interactions */
 import React from 'react';
+import Router from 'next/router';
 import Link from 'next/link';
 import { Typography, Row, Col, Card } from 'antd';
 
@@ -9,23 +12,32 @@ const { Title } = Typography;
 const { courseCard, mt4 } = CLASS_NAMES;
 const { courseSelectLabel } = CHALLENGE_PAGES_HEADERS;
 
-const CourseSelect = ({ subjects, next }) => (
-    <section>
-        <Title level={3}>{courseSelectLabel}</Title>
-        <Row gutter={16} className={mt4}>
-            {subjects.map(({ name, id }) => (
-                <Col key={id} span={6} md={6} xs={12}>
-                    <Link href={next}>
-                        <Card className={courseCard} hoverable>
-                            {name}
-                        </Card>
-                    </Link>
-                </Col>
-            ))}
+const CourseSelect = ({ subjects, next, selectCourse }) => {
+    const handleSelect = subject => {
+        selectCourse(subject);
+        console.log(next);
+        Router.replace(next, { shallow: true });
+    };
 
-        </Row>
-    </section>
-);
+    return (
+        <section>
+            <Title level={3}>{courseSelectLabel}</Title>
+            <Row gutter={16} className={mt4}>
+                {subjects.map(({ name, id }) => (
+                    <Col key={id} span={6} md={6} xs={12}>
+                        {/* <Link href={next}> */}
+                        <div onClick={() => handleSelect({ id, name })}>
+                            <Card className={courseCard} hoverable>
+                                {name}
+                            </Card>
+                        </div>
+                        {/* </Link> */}
+                    </Col>
+                ))}
+            </Row>
+        </section>
+    );
+};
 
 CourseSelect.defaultProps = {
     subjects: DEFAULT_PROPS.courseSelect.subjects,
