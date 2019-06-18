3c5ab104090fe379f88234da8a27d1fe01eb2ebe
teenoh <ugbalaachinike@gmail.com>
2019-06-18 17:18:17 +0100

    reducers and sagas for challenge req status

--- components/challenge/reducers.js
+++ components/challenge/reducers.js
@@ -2,15 +2,21 @@ import {
     SELECT_COURSE_ACTION_TYPES,
     SELECT_MODE_ACTION_TYPES,
     SELECT_OPPONENT_ACTION_TYPES,
-    SET_ONLINE_USERS_ACTION_TYPES
+    SET_ONLINE_USERS_ACTION_TYPES,
+    SET_CHALLENGE_REQ_STATUS_ACTION_TYPES
 } from './actionTypes';
 
 const { SELECT_COURSE } = SELECT_COURSE_ACTION_TYPES;
 const { SELECT_MODE } = SELECT_MODE_ACTION_TYPES;
 const { SELECT_OPPONENT } = SELECT_OPPONENT_ACTION_TYPES;
 const { SET_ONLINE_USERS } = SET_ONLINE_USERS_ACTION_TYPES;
+const { SET_CHALLENGE_REQ_STATUS } = SET_CHALLENGE_REQ_STATUS_ACTION_TYPES;
 
 const initialState = {
+    // challengeReqStatus
+    // can either be 'pending', 'approved' or 'rejected'
+    // it is populated when a Challenge Request is made
+    challengeReqStatus: null,
     courses: [],
     onlineUsers: [],
     selectedCourse: null,
@@ -43,6 +49,12 @@ export default (state = initialState, action) => {
         return {
             ...state,
             onlineUsers: action.payload,
+        };
+
+    case SET_CHALLENGE_REQ_STATUS:
+        return {
+            ...state,
+            challengeReqStatus: action.payload,
         };
 
     default:
--- components/challenge/sagas.js
+++ components/challenge/sagas.js
@@ -5,13 +5,15 @@ import {
     selectCourseAction,
     selectModeAction,
     selectOpponentAction,
-    setOnlineUsersAction
+    setOnlineUsersAction,
+    setChallengeReqStatusAction
 } from './actions';
 import {
     SELECT_COURSE_ACTION_TYPES,
     SELECT_MODE_ACTION_TYPES,
     SELECT_OPPONENT_ACTION_TYPES,
-    SET_ONLINE_USERS_ACTION_TYPES
+    SET_ONLINE_USERS_ACTION_TYPES,
+    SET_CHALLENGE_REQ_STATUS_ACTION_TYPES
 } from './actionTypes';
 import { selectors } from '../auth';
 
@@ -19,6 +21,7 @@ const { SELECT_COURSE_REQUEST } = SELECT_COURSE_ACTION_TYPES;
 const { SELECT_MODE_REQUEST } = SELECT_MODE_ACTION_TYPES;
 const { SELECT_OPPONENT_REQUEST } = SELECT_OPPONENT_ACTION_TYPES;
 const { SET_ONLINE_USERS_REQUEST } = SET_ONLINE_USERS_ACTION_TYPES;
+const { SET_CHALLENGE_REQ_STATUS_REQUEST } = SET_CHALLENGE_REQ_STATUS_ACTION_TYPES;
 const { getUser } = selectors;
 
 function* selectCourse(action){
@@ -56,6 +59,16 @@ function* setOnlineUsers(action) {
         const onlineUsers = action.payload.filter(({ id }) => user._id !== id);
         console.log(onlineUsers);
         yield put(setOnlineUsersAction(onlineUsers));
+    }
+    catch (e) {
+        //
+    }
+};
+
+function* setChallengeReqStatus(action) {
+    console.log('it got to set challenge')
+    try {
+        yield put(setChallengeReqStatusAction(action.payload));
     }
     catch (e) {
         //
@@ -101,12 +114,22 @@ function* watchSetOnlineUsers() {
     }
 };
 
+function* watchSetChallengeReqStatus() {
+    try {
+        yield takeLatest(SET_CHALLENGE_REQ_STATUS_REQUEST, setChallengeReqStatus);
+    }
+    catch (e) {
+        //
+    }
+};
+
 export default function* (){
     yield all([
         watchSelectCourse(),
         watchSelectMode(),
         watchSelectOpponent(),
         watchSetOnlineUsers(),
+        watchSetChallengeReqStatus(),
     ]);
 }
 
