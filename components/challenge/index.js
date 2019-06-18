9833b4dbce71badf56b3b6da2a00dd607d57cc28
teenoh <ugbalaachinike@gmail.com>
2019-06-18 11:58:45 +0100

    add selectors for accessing store in challenge component

--- components/challenge/index.js
+++ components/challenge/index.js
@@ -2,7 +2,8 @@
 import * as components from './components';
 import * as constants from './constants';
 import * as actions from './actions';
+import * as selectors from './selectors';
 import reducers from './reducers';
 import sagas from './sagas';
 
-export { actions, components, constants, reducers, sagas };
+export { actions, components, constants, reducers, sagas, selectors };
---
+++ components/challenge/selectors.js
@@ -0,0 +1,5 @@
+import { NAME } from './constants';
+
+export const getOnlineUsers = state => state[NAME].onlineUsers;
+
+export const getCourses = state => state[NAME].courses;
