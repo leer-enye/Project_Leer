37688b5c68a90126b1d2d610c4485c75abd34f9d
teenoh <ugbalaachinike@gmail.com>
2019-06-18 11:59:04 +0100

    add sagas for setting online users

--- components/challenge/sagas.js
+++ components/challenge/sagas.js
@@ -1,20 +1,23 @@
 import { all, put, takeLatest, select } from 'redux-saga/effects';
-import axios from 'axios';
+// import axios from 'axios';
 
 import {
     selectCourseAction,
     selectModeAction,
-    selectOpponentAction
+    selectOpponentAction,
+    setOnlineUsersAction
 } from './actions';
 import {
     SELECT_COURSE_ACTION_TYPES,
     SELECT_MODE_ACTION_TYPES,
-    SELECT_OPPONENT_ACTION_TYPES
+    SELECT_OPPONENT_ACTION_TYPES,
+    SET_ONLINE_USERS_ACTION_TYPES
 } from './actionTypes';
 
 const { SELECT_COURSE_REQUEST } = SELECT_COURSE_ACTION_TYPES;
 const { SELECT_MODE_REQUEST } = SELECT_MODE_ACTION_TYPES;
 const { SELECT_OPPONENT_REQUEST } = SELECT_OPPONENT_ACTION_TYPES;
+const { SET_ONLINE_USERS_REQUEST } = SET_ONLINE_USERS_ACTION_TYPES;
 
 function* selectCourse(action){
     try {
@@ -42,6 +45,17 @@ function* selectOpponent(action) {
         //
     }
 };
+
+function* setOnlineUsers(action) {
+    try {
+        yield put(setOnlineUsersAction(action.payload));
+    }
+    catch (e) {
+        //
+    }
+};
+
+// WATCHERS
 
 function* watchSelectCourse(){
     try{
@@ -71,11 +85,21 @@ function* watchSelectOpponent() {
     }
 };
 
+function* watchSetOnlineUsers() {
+    try {
+        yield takeLatest(SET_ONLINE_USERS_REQUEST, setOnlineUsers);
+    }
+    catch (e) {
+        //
+    }
+};
+
 export default function* (){
     yield all([
         watchSelectCourse(),
         watchSelectMode(),
         watchSelectOpponent(),
+        watchSetOnlineUsers(),
     ]);
 }
 
