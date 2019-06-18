27bd9bbf85c73bc514b2d64b73751bbff84f4f5c
teenoh <ugbalaachinike@gmail.com>
2019-06-18 19:01:12 +0100

    fetch courses only when courses in store is empty

--- pages/admin/challenge/index.js
+++ pages/admin/challenge/index.js
@@ -1,21 +1,40 @@
 import React from 'react';
 import { connect } from 'react-redux';
 
-import { components, actions } from '../../../components/challenge';
+import { components, actions, selectors } from '../../../components/challenge';
 import { constants } from '../../../components/common';
 import withAuthSync from '../../../hocs/withAuthSync';
 
-const { selectCourseRequest } = actions;
+const { fetchCoursesRequest ,selectCourseRequest } = actions;
 const { NEXT_LINKS } = constants;
 const { modeSelectLink } = NEXT_LINKS;
 const { CourseSelect } = components;
+const { getCourses } = selectors;
 
-const ChallengeHome = ({ selectCourse }) => (
-    <CourseSelect selectCourse={selectCourse} next={modeSelectLink} />
-);
+class ChallengeHome extends React.Component {
+    state = {}
+
+    componentWillMount(){
+        const { courses, fetchCourses } = this.props;
+
+        if (courses.length === 0 ){
+            fetchCourses();
+        }
+    }
+
+    render(){
+        const { courses, selectCourse } = this.props;
+        return <CourseSelect courses={courses} selectCourse={selectCourse} next={modeSelectLink} />;
+    }
+}
+
+const mapStateToProps = state => ({
+    courses: getCourses(state),
+});
 
 const mapDispatchToProps = dispatch => ({
+    fetchCourses: () => dispatch(fetchCoursesRequest()),
     selectCourse: data => dispatch(selectCourseRequest(data)),
 });
  
-export default withAuthSync(connect(null, mapDispatchToProps)(ChallengeHome));
+export default withAuthSync(connect(mapStateToProps, mapDispatchToProps)(ChallengeHome));
